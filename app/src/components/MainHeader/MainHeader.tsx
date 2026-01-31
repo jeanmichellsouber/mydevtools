ebc6086b649e311b1b2c5f3c4f76697d78766124
Jean Michell <jeanmichellsouber@gmail.com>
2026-01-31 15:42:07 -0300

    more progress

--- app/src/components/MainHeader/MainHeader.tsx
+++ app/src/components/MainHeader/MainHeader.tsx
@@ -1,9 +1,12 @@
 import { StyledMainHeader } from './MainHeader.styles';
+import MyDevToolsLogo from '../../assets/images/mydevtools.svg';
 
 export const MainHeader = () => {
   return (
     <StyledMainHeader>
-      <div>Logo</div>
+      <div>
+        <img src={MyDevToolsLogo} title="MY_DEV Tools" />
+      </div>
       <div>Theme switcher</div>
     </StyledMainHeader>
   );
