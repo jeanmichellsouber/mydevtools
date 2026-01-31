4a25cc4c5a5b13cffcd63f026279b2bfc144440d
Jean Michell <jeanmichellsouber@gmail.com>
2026-01-31 15:06:05 -0300

    initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { MainHeader } from './components/MainHeader';
+
+const App = () => {
+  //logic
+
+  return (
+    <>
+      <MainHeader />
+    </>
+  );
+};
+
+export default App;
---
+++ app/src/components/MainHeader/MainHeader.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledMainHeader = styled.header`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background: linear-gradient(to right, #2d323a, #006996);
+    color: #fff;
+  }
+`;
