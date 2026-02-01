aa451387dd6be0d2b8796d840dff28639d8db1d1
Jean Michell <jeanmichellsouber@gmail.com>
2026-02-01 00:26:50 -0300

    enhancements

---
+++ app/src/components/BlankWrapper/BlankWrapper.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledBlankWrapper = styled.div`
+  background: #fff;
+  width: 870px;
+  border-radius: 10px;
+  margin: 30px auto;
+  border: solid 1px #c9c9c9;
+  padding: 30px;
+  min-height: 300px;
+`;
